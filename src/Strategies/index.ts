620d54a6557352bcc6582673963454e141383581
SamHamamji <samhamamji0@gmail.com>
2022-12-23 17:33:22 +0200

    Export strategies in a single object

--- src/Strategies/index.ts
+++ src/Strategies/index.ts
@@ -1,6 +1,15 @@
-export { default as AlwaysFirst } from "./AlwaysFirst";
-export { default as AlwaysLast } from "./AlwaysLast";
-export { default as Random } from "./Random";
-export { default as UserInput } from "./UserInput";
-export { default as ChooseMaximum } from "./ChooseMaximum";
-export { default as Minimax } from "./Minimax";
+import AlwaysFirst from "./AlwaysFirst";
+import AlwaysLast from "./AlwaysLast";
+import Random from "./Random";
+import UserInput from "./UserInput";
+import ChooseMaximum from "./ChooseMaximum";
+import Minimax from "./Minimax";
+
+export default {
+    AlwaysFirst,
+    AlwaysLast,
+    Random,
+    UserInput,
+    ChooseMaximum,
+    Minimax,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,32 @@
 import colors from "colors/safe";
 import Game from "./Game";
 import Player from "./Player";
-import { Minimax, Random, UserInput } from "./Strategies";
+import Strategies from "./Strategies";
+import Minimax from "./Strategies/Minimax";
 
-// const board = [11, 1, 5, 4, 11, 13, 4, 2].map((element: number) => new Card(element, CARD_SYMBOLS.CLUBS));
 const cardsNumber = 12;
 const board = Game.generateCards(cardsNumber);
 
 const players = [
-    new Player("Bot 1", new Minimax(), colors.yellow),
-    new Player("Player 1", new UserInput(), colors.red),
-    new Player("Bot 2", new Random(), colors.blue),
-    new Player("Player 2", new UserInput(), colors.green),
+    new Player("Bot 1", new Strategies.Minimax(), colors.yellow),
+    new Player("Player 1", new Strategies.UserInput(), colors.red),
+    new Player("Bot 2", new Strategies.Random(), colors.blue),
+    new Player("Player 2", new Strategies.UserInput(), colors.green),
 ];
 
 const game = new Game(players, board);
 const minimax = players[0].strategy as Minimax;
 
-async function main(){
+async function main() {
     const timeDelay = 1000;
     await game.play(timeDelay);
     console.log(game.endScreen());
-    
+
     console.log("\nInitial Nash equilibrium:");
-    const nashEquilibrium = minimax.nashEquilibrium(game, 
+    const nashEquilibrium = minimax.nashEquilibrium(game,
         { first: 0, last: cardsNumber - 1 }
     );
-    console.log(nashEquilibrium.map((score, index)=>
+    console.log(nashEquilibrium.map((score, index) =>
         `${players[index].colorizedName}: ${score}`
     ).join("\n"));
 }
